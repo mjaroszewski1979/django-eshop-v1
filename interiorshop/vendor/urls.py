385982c0eb6c128ab72495e0d542930afa5788a1
Maciej Jaroszewski <mjaroszewski1979@gmail.com>
2021-10-28 20:50:26 +0200

    adding products

--- interiorshop/vendor/urls.py
+++ interiorshop/vendor/urls.py
@@ -4,8 +4,9 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('become_vendor', views.become_vendor, name='become_vendor'),
-    path('vendor_admin', views.vendor_admin, name='vendor_admin'),
+    path('become_vendor/', views.become_vendor, name='become_vendor'),
+    path('vendor_admin/', views.vendor_admin, name='vendor_admin'),
+    path('add_product/', views.add_product, name='add_product'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('login/', auth_views.LoginView.as_view(template_name='vendor/login.html'), name='login')
 ]
--- interiorshop/vendor/views.py
+++ interiorshop/vendor/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
+from django.utils.text import slugify
 
 from .models import Vendor
 from product.models import Product
+
+from .forms import ProductForm
 
 def become_vendor(request):
     if request.method == 'POST':
@@ -29,3 +32,21 @@ def vendor_admin(request):
     products = vendor.products.all()
 
     return render(request, 'vendor/vendor_admin.html', {'vendor': vendor, 'products': products})
+
+@login_required
+def add_product(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            product = form.save(commit=False)
+            product.vendor = request.user.vendor
+            product.slug = slugify(product.title)
+            product.save()
+
+            return redirect('vendor_admin')
+
+    else:
+        form = ProductForm()
+
+    return render(request, 'vendor/add_product.html', {'form': form})
