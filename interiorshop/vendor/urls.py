c4cdb2a0644b548fdd42a7668fc00c4cf256e477
Maciej Jaroszewski <mjaroszewski1979@gmail.com>
2021-10-28 18:28:02 +0200

    first commit

---
+++ interiorshop/vendor/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+
+from . import views
+
+urlpatterns = [
+    path('become_vendor', views.become_vendor, name='become_vendor'),
+    path('vendor_admin', views.vendor_admin, name='vendor_admin'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('login/', auth_views.LoginView.as_view(template_name='vendor/login.html'), name='login')
+]
